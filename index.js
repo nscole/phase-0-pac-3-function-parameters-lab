83949987834d5299c3f2fd33c13a4dc23aa678b1
Naomi Cole <naomisarahcole@gmail.com>
2022-05-26 12:15:34 +1000

    Completed tests

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function introduction (name="Josh") {
+   return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage (name="Josh",language="Ember.js") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+ }
+
+ function introductionWithLanguageOptional (name="Gracie",language="JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+ }
